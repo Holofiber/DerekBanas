4297f33a5244dfcaeb3936149c04400a8876a0ba
Holofiber <aminus2010@gamil.com>
2017-10-18 19:33:52 +0300

    DerekBanas: if else

--- Tutorial3/Program.cs
+++ Tutorial3/Program.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tutorial3
+namespace Tutorial_3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int age = 17;
+
+                if ((age >= 5)&&(age <=7))
+            {
+                Console.WriteLine("Go to Elemebntary Shcool");
+            }
+                else if ((age >7)&&(age <13))
+            {
+                Console.WriteLine( "Go to Middel shcool");
+            }
+                else if ((age>13)&&(age <19))
+            {
+                Console.WriteLine("Go to High shcool");
+            }
+                else
+            {
+                Console.WriteLine("Go to College");
+            }
+
+                if ((age<14)||(age <19))
+            {
+                Console.WriteLine("You Shouldn't Work");
+            }
+
+            Console.WriteLine( "! true = " + (!true));
+
+
+            Console.ReadLine();
         }
     }
 }
