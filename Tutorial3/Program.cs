ef0bd3aacd08dbd9d2853795a04894cd547f6488
Holofiber <aminus2010@gamil.com>
2017-10-20 12:34:29 +0300

    DB: Tutorial_4 Methods Enum

--- Tutorial3/Program.cs
+++ Tutorial3/Program.cs
@@ -12,32 +12,131 @@ namespace Tutorial_3
         {
             int age = 17;
 
-                if ((age >= 5)&&(age <=7))
+            if ((age >= 5) && (age <= 7))
             {
                 Console.WriteLine("Go to Elemebntary Shcool");
             }
-                else if ((age >7)&&(age <13))
+            else if ((age > 7) && (age < 13))
             {
-                Console.WriteLine( "Go to Middel shcool");
+                Console.WriteLine("Go to Middel shcool");
             }
-                else if ((age>13)&&(age <19))
+            else if ((age > 13) && (age < 19))
             {
                 Console.WriteLine("Go to High shcool");
             }
-                else
+            else
             {
                 Console.WriteLine("Go to College");
             }
 
-                if ((age<14)||(age <19))
+            if ((age < 14) || (age < 19))
             {
                 Console.WriteLine("You Shouldn't Work");
             }
 
-            Console.WriteLine( "! true = " + (!true));
+            Console.WriteLine("! true = " + (!true));
+
+            bool canDrive = age >= 16 ? true : false;
+            //bool canDrive = age >= 16;
+
+            switch (age)
+            {
+                case 1:
+                case 2:
+                    Console.WriteLine("Go to Day Care");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Go to Preschool");
+                    break;
+                case 5:
+                    Console.WriteLine("Go to another school");
+                    break;
+                default:
+                    Console.WriteLine("go to Otherschool");
+                    goto Otherschool;
+
+            }
+
+            Otherschool:
+            Console.WriteLine("Elementary, Middle, High");
+
+            string name = "Derek"; 
+            string name2 = "derek";
+
+            if (name.Equals(name2, StringComparison.Ordinal))
+            {
+                Console.WriteLine("Names are Equal");
+            }
+
+            int i = 1;
+            while (i <= 10)
+            {
+                if (i % 2 == 0)
+                {
+                    i++;
+                    continue;
+                }
+
+                if (i == 9) break;
+
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, 11);
+            int numberGuessed = 0;
+
+            do
+            {
+                Console.WriteLine("Enter a number between 1 & 10 : ");
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
+            }
+            while (secretNumber != numberGuessed);
+
+            double num1 = 5;
+            double num2 = 0;
+            try
+            {
+                Console.WriteLine("5 / 0 = {0}", DoDivision(num1, num2));
+            }
+
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You can't divide by zero");
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("You can't divide by zero");
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("Cleaning Up");
+            }
 
 
             Console.ReadLine();
         }
+
+        static double DoDivision (double x, double y)
+        {
+            if(y == 0)
+            {
+                throw new System.DivideByZeroException();
+            }
+
+            return x / y;
+
+        }
+
+
+
     }
 }
---
+++ Tutorial_4/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 5;
+            double y = 4;
+            Console.WriteLine("5+4={0}", GetSum(x, y));
+            Console.WriteLine("x " + x);
+
+            int solution;
+
+            Doublelt(15, out solution);
+            Console.WriteLine("15 * 2 = {0}", solution);
+
+            int num1 = 10;
+            int num2 = 20;
+            Console.WriteLine("Before Swap num1 : {0} num2 :{1}", num1, num2);
+            Swap(ref num1, ref num2);
+            Console.WriteLine("After Swap num1 : {0} num2 :{1}", num1, num2);
+
+            Console.WriteLine("1+2+3={0}", GetSumeMore(1, 2, 3));
+
+            PrintInfo(zipCode: 15147, name: "Derek");
+
+            Console.WriteLine("5.0 + 4.5 = {0}", GetSum(5.0, 4.5));
+
+            Console.WriteLine("5.0 + 4.5 = {0}", GetSum("5,0", "4,5"));
+
+            CarColor car1 = CarColor.Red;
+            PaintCar(car1);
+
+            Console.ReadLine();
+        }
+        static double GetSum(double x = 1, double y = 1)
+        {
+            double temp = x;
+            x = y;
+            y = temp;
+            return x + y;
+        }
+
+        static double GetSum(string x = "1", string y = "1")
+        {
+
+            double dblX = Convert.ToDouble(x);
+            double dblY = Convert.ToDouble(y);
+                       
+            return dblX + dblY;
+        }
+
+        static void Doublelt(int x, out int solution)
+        {
+            solution = x * 2;
+        }
+
+        public static void Swap(ref int num1, ref int num2)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        static double GetSumeMore(params double[] nums)
+        {
+            double sum = 0;
+            foreach (int i in nums)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        static void PrintInfo(string name, int zipCode)
+        {
+            Console.WriteLine("{0} lives in the zip code {1}", name, zipCode);
+        }
+
+        enum CarColor : byte
+        {
+            Orange = 1,
+            Blue,
+            Gereen,
+            Red,
+            Yellow
+        }
+
+        static void PaintCar (CarColor cc)
+        {
+            Console.WriteLine("The car was painted {0} with the code {1}", cc, (int)cc);
+        }
+    }
+}
