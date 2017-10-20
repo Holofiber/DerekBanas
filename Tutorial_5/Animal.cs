9c77b9bf9a4e10a96003361258ef229b79249067
Holofiber <aminus2010@gamil.com>
2017-10-20 20:05:11 +0300

    DB: Tutorial_5 Classes & OOP

---
+++ Tutorial_5/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Tutorial_5
+{
+    class Animal
+    {
+
+        public string name;
+        public string sound;
+
+        public Animal()
+        {
+            name = "No name";
+            sound = "No Sound";
+            numOfAnimals++;
+        }
+
+        public Animal (string name = "No Name", string sound = "No Sound")
+        {
+            this.name = name;
+            this.sound = sound;
+            numOfAnimals++;
+
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says {1}", name, sound);
+        }
+
+        static int numOfAnimals = 0;
+
+        public static int GetNumAnimals()
+        {
+            return numOfAnimals;
+        }
+    }
+}
---
+++ Tutorial_5/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? randNum = null;
+
+            if (randNum == null)
+            {
+                Console.WriteLine("randNum is null");
+            }
+
+            if (!randNum.HasValue)
+            {
+                Console.WriteLine("randNum is null");
+            }
+
+
+            Console.WriteLine("Area of Rectangle : {0}", ShapeMath.GetArea("rectangle", 5, 6));
+
+            
+            Animal fox = new Animal()
+            {
+                name = "Red",
+                sound = "Raww"
+            };
+
+            fox.MakeSound();
+
+
+            Console.WriteLine("# of Animals : {0}", Animal.GetNumAnimals());
+
+            Rectangle rect1;
+            rect1.lenght = 200;
+            rect1.width = 50;
+            Console.WriteLine("Area of rect1 : {0}", rect1.Area());
+
+            Rectangle rect2 = new Rectangle(100, 40);
+
+            rect2 = rect1;
+            rect1.lenght = 33;
+
+            Console.WriteLine("rect2.lenght : {0}", rect2.lenght);
+
+            Console.ReadLine();
+        }
+
+        struct Rectangle
+        {
+            public double lenght;
+            public double width;
+
+            public Rectangle(double l = 1, double w = 1)
+            {
+                lenght = l;
+                width = w;
+            }
+
+            public double Area()
+            {
+                return lenght * width;
+            }
+        }
+    }
+}
