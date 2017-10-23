3c92b39c74f8ea0c6aadc73eec1f8e3efe70109e
Holofiber <aminus2010@gamil.com>
2017-10-23 21:08:00 +0300

    DB:  C# Tutorial 6 Constructors Properties

---
+++ Tutorial_6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal();
+
+            cat.SetName("Whiskers");
+            cat.Sound = "Meow";
+
+            Console.WriteLine("The cat is named {0} and says {1}", cat.GetName(), cat.Sound);
+
+            cat.Owner = "Derek";
+
+            Console.WriteLine("{0} owner is {1}", cat.GetName(), cat.Owner);
+            Console.WriteLine("{0} shelter is {1}", cat.GetName(), cat.idNum);
+
+            Animal fox = new Animal("Oscar", "Raweerr");
+
+            Console.WriteLine("# of Animals : {0}", Animal.NumOfAnimals);
+
+            Console.Read();
+
+        }
+    }
+}
