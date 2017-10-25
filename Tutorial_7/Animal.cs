d8b2fe4ad639eba1724455bbe64ad5dc79fd6b69
Holofiber <aminus2010@gamil.com>
2017-10-25 18:57:32 +0300

    DB: Tutorial 7 Inheritance Polymorphism

---
+++ Tutorial_7/Animal.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using Tutorial_7;
+
+
+namespace Tutorial_5
+{
+    class Animal
+    {
+
+        private string name;
+        protected string sound;
+
+        protected AnimalIDInfo animalIdInfo = new AnimalIDInfo();
+
+        public void SetAnimalIDinfo(int idNum, string owner)
+        {
+            animalIdInfo.IDNum = idNum;
+            animalIdInfo.Owner = owner;
+        }
+
+        public void GetAnimalIDInfo()
+        {
+            Console.WriteLine($"{Name} had the ID of {animalIdInfo.IDNum} and is owned by {animalIdInfo.Owner}");
+        }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine($"{Name} says {Sound}");
+        }
+
+        public Animal():this ("No Name", "No Sound") { }
+
+        public Animal(string name):this(name, "No sound") { }
+
+        public Animal(string name, string sound)
+        {
+            Name = name;
+            Sound = sound;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!value.Any(char.IsDigit))
+                {
+                    name = "No Name";
+                }
+                name = value;
+            }
+        }
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if (value.Length >10)
+                {
+                    name = "No Sound";
+                }
+                sound = value;
+            }
+        }
+
+        public class AnimalHealth
+        {
+            public bool HealtWeight(double height, double weight)
+            {
+                double calc = height / weight;
+
+                if ((calc >= .18) && (calc <= .27))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Tutorial_7/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorial_5;
+
+namespace Tutorial_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal whiskers = new Animal()
+            {
+                Name = "Whiskers",
+                Sound = "Meow"
+            };
+
+            Dog grover = new Dog()
+            {
+                Name = "Grover",
+                Sound = "Woof",
+                Sound2 = "Grrr"
+            };
+            grover.Sound = "Wooooof";
+
+            whiskers.MakeSound();
+            grover.MakeSound();
+
+            whiskers.SetAnimalIDinfo(12345, "Sally Smith");
+
+            grover.SetAnimalIDinfo(12346, "Paul Brown");
+
+            whiskers.GetAnimalIDInfo();
+            grover.GetAnimalIDInfo();
+
+            Animal.AnimalHealth getHealth = new Animal.AnimalHealth();
+
+            Console.WriteLine("Is my animal healthy : {0}",
+            getHealth.HealtWeight(11, 146));
+
+            Animal monkey = new Animal()
+            {
+                Name = "Happy",
+                Sound = "Eeeeee"
+            };
+
+            Animal spot = new Dog()
+            {
+                Name = "Spot",
+                Sound = "Wooof",
+                Sound2 = "Geeerrrr"
+            };
+
+            monkey.MakeSound();
+            spot.MakeSound();
+
+            Console.ReadLine();
+        }
+    }
+}
