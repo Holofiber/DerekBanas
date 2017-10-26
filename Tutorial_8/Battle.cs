37adc776c898d1ed24fbbd8e4be1a21e2b76d12b
Holofiber <aminus2010@gamil.com>
2017-10-26 19:39:37 +0300

    DB: C# Tutorial 8 OOP Game

---
+++ Tutorial_8/Battle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_8
+{
+    class Battle
+    {
+        public static void StartFight(Warrior warrior1, Warrior warrior2)
+        {
+            while (true)
+            {
+                if (GetAttackResult(warrior1, warrior2) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+
+                if (GetAttackResult(warrior2, warrior1) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+            }
+
+
+        }
+
+        public static string GetAttackResult(Warrior warriorA, Warrior warriorB)
+        {
+            double warAAttkAmt = warriorA.Attack();
+            double warBBlockAmt = warriorB.Block();
+            double dmg2WarB = warAAttkAmt - warBBlockAmt;
+
+            if (dmg2WarB > 0)
+            {
+                warriorB.Health = warriorB.Health - dmg2WarB;
+            }
+            else dmg2WarB = 0;
+            {
+                Console.WriteLine("{0} Attacks {1} and Deals {2} Demage", warriorA.Name, warriorB.Name, dmg2WarB);
+            }
+
+            Console.WriteLine("{0} Has {1} Health \n", warriorB.Name, warriorB.Health);
+
+            if (warriorB.Health <= 0)
+            {
+                Console.WriteLine("{0} has Died and {1} is Victorius \n", warriorB.Name, warriorA.Name);
+                return "Game Over";
+            }
+            else return "Fight Again";
+        }
+    }
+}
---
+++ Tutorial_8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior maximus = new Warrior("Maximus", 10,3,1);
+
+            Warrior bob =  new Warrior("Bob", 10, 3,1);
+
+            Battle.StartFight(maximus, bob);
+
+
+            Console.ReadLine();
+        }
+    }
+}
