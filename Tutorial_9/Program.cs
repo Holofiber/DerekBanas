f93af1e5cc0f027e61d2a3fe4cc7726c8304fdc1
Holofiber <aminus2010@gmail.com>
2017-11-06 18:29:18 +0200

    DB: Tutorail 9 Abstract Class Polymorphism

---
+++ Tutorial_9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = {new Circle(5), new Rectangle(4, 5)};
+
+            foreach (Shape s in shapes)
+            {
+                s.GetInfo();
+                Console.WriteLine("{0} Area : {1:f2}", s.Name, s.Area());
+                Circle tesCircle = s as Circle;
+                if (tesCircle == null)
+                {
+                    Console.WriteLine("This isn't a Circle");
+                }
+                if (s is Circle)
+                {
+                    Console.WriteLine("This isn't a Rectangle");
+                }
+                Console.WriteLine();
+                object circl1 = new Circle(4);
+                Circle circl2 = (Circle) circl1;
+
+                Console.WriteLine("The {0} Area is {1:f2}", circl2.Name, circl2.Area());
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+        
+    
+}
---
+++ Tutorial_9/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_9
+{
+    class Rectangle : Shape
+    {
+        public  double Lenght { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double lenght, double width)
+        {
+            Name = "Rectanqle";
+            Lenght = lenght;
+            Width = width;
+        }
+        public override double Area()
+        {
+            return Lenght * Width;
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"It has a Lenght of {Lenght} and Width of {Width}");
+        }
+    }
+}
