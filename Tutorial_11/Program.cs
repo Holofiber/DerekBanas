858b1540efb0dfea486ea170ea4c71e5bebfa866
SPro <aminus2010@gmail.com>
2017-11-09 14:03:43 +0200

    DB: Tutorail 11 p1

---
+++ Tutorial_11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Tutorial_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ArrayList Code
+
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add("Bob");
+            arrayList.Add(40);
+
+            Console.WriteLine("Count: {0}", arrayList.Count);
+
+            Console.WriteLine("Capacity: {0}", arrayList.Capacity);
+            
+
+            ArrayList arrayList2 = new ArrayList();
+
+            arrayList2.AddRange(new object[] { "Mike", "Sally", "Egg" });
+
+            arrayList.AddRange(arrayList2);
+
+            arrayList2.Sort();
+            arrayList2.Reverse();
+
+            arrayList2.Insert(1, "Turkey");
+
+            ArrayList range = arrayList2.GetRange(0, 2);
+
+            foreach (object o in range)
+            {
+                Console.WriteLine(o);
+            }
+
+            #endregion
+
+
+            Console.ReadLine();
+        }
+    }
+}
