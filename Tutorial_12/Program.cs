350114414d569dbbae5a839d1b4f7c75c2489a23
SPro <aminus2010@gmail.com>
2017-11-10 16:54:28 +0200

    DB: 12.1

---
+++ Tutorial_12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animalList = new List<Animal>();
+            animalList.Add(new Animal() { Name = "Doug" });
+            animalList.Add(new Animal() { Name = "Paul" });
+            animalList.Add(new Animal() { Name = "Sally" });
+
+            animalList.Insert(1, new Animal() { Name = "Steve" });
+            animalList.RemoveAt(1);
+            Console.WriteLine("Num of Animals : {0}", animalList.Count())
+
+            Console.ReadLine();
+        }
+    }
+}
